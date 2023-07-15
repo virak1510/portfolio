d9eea022d2e05c70037199f574e173543b1be18d
virak <virak.1510@gmail.com>
2023-07-15 22:24:17 +0700

    fix

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -4,12 +4,12 @@ import { PUBLIC_POCKETBASE_URL } from "$env/static/public";
 import { error } from "@sveltejs/kit";
 const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
 export async function load() {
-    try {
-        const record = await pb.collection('theme').getFirstListItem('id="eygsnhbtpaybi3l"');
-        let theme = record.theme;
-        return {theme}
-    } catch (err) {
-        throw error(500, err.message)
-    }
-    return {theme};
+    // try {
+    //     const record = await pb.collection('theme').getFirstListItem('id="eygsnhbtpaybi3l"');
+    //     let theme = record.theme;
+    //     return {theme}
+    // } catch (err) {
+    //     throw error(500, err.message)
+    // }
+    // return {theme};
 };
